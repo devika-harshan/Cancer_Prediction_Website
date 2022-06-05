0b320ab305f9fba389454eade5b9cb9d9e5aff34
Devika Harshan <85967317+devika-harshan@users.noreply.github.com>
2022-06-05 13:48:54 +0530

    Intial commmit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask,render_template,request,redirect
+
+
+
+app = Flask(__name__)
+
+
+
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/help')
+def help():
+    return render_template("help.html")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
